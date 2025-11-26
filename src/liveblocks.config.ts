db99e3a9d1272029d5fce16b1d00b04fe3c370e0
Aayush <aayush@example.com>
2025-11-26 10:22:38 +0530

    Update Liveblocks public API key

--- src/liveblocks.config.ts
+++ src/liveblocks.config.ts
@@ -3,5 +3,5 @@ import { createClient } from "@liveblocks/client";
 
 // Create a client with your public API key
 export const client = createClient({
-  publicApiKey: "pk_dev_KEl8EI7u9XS64mmj_71n_YLbS7DOBolY6sQnEbIQBBc8-g1-m0tzXtV6ggEK-SvX",
+  publicApiKey: "pk_dev_zNvuLkpUTPxdr1ArXVbb0rHjG67Ic0aPcFL9ZwyjhYNJ2B3skHYUWlIhb5jLKeoX",
 });
